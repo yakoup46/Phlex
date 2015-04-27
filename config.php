a94d078ac34a4ee291ab1ebb198278cd35c36a39
Jacob Luta <jluta@dattobackup.com>
2015-04-27 09:42:04 -0400

    check if file exists on autoloader

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
 
 spl_autoload_register(function($class) {
+    $path = 'controllers/' . $class . '.php';
+
+    if (!file_exists($path)) {
+        exit('Class Name Doesn\'t Match File Name');
+    }
+
     require_once 'controllers/' . $class . '.php';
 });
